ff759b5d0d36173486c4d37f087efaa3b186f0c4
OtarKebadze <OtarKebadze@gmail.com>
2022-05-04 23:10:43 -0300

    cambios

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-let fs = require ("fs")
+let fs = require("fs");
 class Contenedor {
-    constructor (nombreArchivo){
-    this.archivo=nombreArchivo
-    }
-    async save(obj){
-        try {
-        
-        let data = await this.getAll()
-        console.log(data)
-        
-        if (!data){
-        obj.id=1;
-        await fs.promises.writeFile(`${this.archivo}`,JSON.stringify([obj]))
-        console.log("ARCHIVO INEXISTENTE, creando...")
-        }else{
-        obj.id=data[data.length-1].id+1;
-        data=[
-        ...data,
-        obj
-        ]
-        await fs.promises.writeFile(`${this.archivo}`,JSON.stringify(data)) 
-        }
-        
-        }catch (error) {
-        console.log(error)
-        }
-        
-    }
-    async getAll(){
-        try {
-        const lectura = await fs.promises.readFile(`${this.archivo}`, "utf-8")
-        //  ESTO ES UN JSON -> console.log(lectura)
-        let arr= await JSON.parse(lectura)
-        return arr
-        } catch (error) {
-        return null
-        }
-    
-    }
-    async getById(id){
-        try {
-        let coincidencia = await this.getAll()
-        .then((resp)=>resp.find(a=>a.id === id))
-        return coincidencia
-        } catch (error) {
-        console.log(error)
-        }
-    }
-    async deleteById(id){
-        try {
-        let coincidencia = await this.getAll()
-        .then((resp)=>resp.filter(a=>a.id !== id))
-        await fs.promises.writeFile(`${this.archivo}`,JSON.stringify(coincidencia)) 
-        } catch (error) {
-        console.log(error)
-        }
-    
-    }
-    async deleteAll(){
+  constructor(nombreArchivo) {
+    this.archivo = nombreArchivo;
+  }
+  async save(obj) {
     try {
-    await fs.promises.writeFile(`${this.archivo}`,"") 
-    } catch (error) {
-    return error
-    }
+      let data = await this.getAll();
 
+      if (!data) {
+        obj.id = 1;
+        await fs.promises.writeFile(`${this.archivo}`, JSON.stringify([obj]));
+        console.log("ARCHIVO INEXISTENTE, creando...");
+        console.log(obj.id);
+      } else {
+        obj.id = data[data.length - 1].id + 1;
+        data = [...data, obj];
+        await fs.promises.writeFile(`${this.archivo}`, JSON.stringify(data));
+        console.log(obj.id);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
+  async getAll() {
+    try {
+      const lectura = await fs.promises.readFile(`${this.archivo}`, "utf-8");
+      //  ESTO ES UN JSON -> console.log(lectura)
+      let arr = await JSON.parse(lectura);
+      return arr;
+    } catch (error) {
+      return null;
+    }
+  }
+  async getById(id) {
+    try {
+      let coincidencia = await this.getAll().then((resp) =>
+        resp.find((a) => a.id === id)
+      );
+      return coincidencia;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteById(id) {
+    try {
+      let coincidencia = await this.getAll().then((resp) =>
+        resp.filter((a) => a.id !== id)
+      );
+      await fs.promises.writeFile(
+        `${this.archivo}`,
+        JSON.stringify(coincidencia)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(`${this.archivo}`, "");
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
-
-
-module.exports=Contenedor
-
-
-
-
+module.exports = Contenedor;
--- test.js
+++ test.js
@@ -1,41 +1,42 @@
-const Contenedor = require("./index")
+const Contenedor = require("./index");
 
-const producto = new Contenedor ("productos.txt")
+const producto = new Contenedor("productos.txt");
 
 /* PRODUCTOS */
-let i=0;
-let arr = [{
-     title: "LENOVO",
-    imagen: "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
+let i = 0;
+let arr = [
+  {
+    title: "LENOVO",
+    imagen:
+      "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
     precio: 200,
-    },
-    {
-        title: "ACER",
-       imagen: "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
-       precio: 400,
-       },
-       {
-        title: "ALIENWARE",
-       imagen: "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
-       precio: 600,
-       }];
-
-
+  },
+  {
+    title: "ACER",
+    imagen:
+      "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
+    precio: 400,
+  },
+  {
+    title: "ALIENWARE",
+    imagen:
+      "https://www.alishop.com.ar/wp-content/uploads/2021/07/legion-Pro-3.jpg",
+    precio: 600,
+  },
+];
 
 /* SE HACE PRUEBA DE TIEMPO, PORQUE SINO SE PISAN*/
 
-
-function intervaloTiempo(){
-let intervalId = setInterval(() => {
-producto.save(arr[i])
-i++
-}, 1000);
-setTimeout(()=>{
-    clearInterval(intervalId)
-},3500)
+function intervaloTiempo() {
+  let intervalId = setInterval(() => {
+    producto.save(arr[i]);
+    i++;
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(intervalId);
+  }, 3500);
 }
 intervaloTiempo();
-
 
 /* METODOS 
     IR USANDO DE A UN METODO PARA EVITAR PROBLEMAS
@@ -48,7 +49,6 @@ intervaloTiempo();
 // producto.deleteById(2)
 // .then((resp)=> console.log(resp));
 
-
 /*descomentar solo si se quiere resetear el txt*/
 
-//producto.deleteAll()
+//producto.deleteAll()f
