33115d28866c5a89e4346cb84e5b3de485ca99ab
OtarKebadze <OtarKebadze@gmail.com>
2022-05-10 14:37:32 -0300

    cambios servidor

--- servidor.js
+++ servidor.js
@@ -4,7 +4,14 @@ const Contenedor = require('./index');
 const producto = require('./test');
 const app = express();
 const port = 8080;
+let html = `
+<h1>PROYECTO NODE OTAR<h1>
+LINKS RECOMENDADOS --->   /productos    ó   /productoRandom
+`
 
+app.get("/", (req,res)=>{
+    res.send(html)
+})
 
 app.get("/productos", (req,res)=>{
 producto.getAll()
