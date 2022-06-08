62e3bae087e4cbd467d8d8d99013f77073be2376
OtarKebadze <OtarKebadze@gmail.com>
2022-06-08 16:51:23 -0300

    cambios

--- router.js
+++ router.js
@@ -1,73 +1,74 @@
 const fs = require("fs");
 const express = require("express");
-const producto = require("./test");
 const router = express.Router();
+const Contenedor = require("./index");
+const producto = new Contenedor("productos.txt");
+const arrayProd = JSON.parse(fs.readFileSync("productos.txt", "utf-8"));
 
-
-// router.get("/", (req, res) => {
-//   res.render('producto')
-// });
-
-router.get("/", async (req, res) => {
- let productos = await producto.getAll() === '' ? '' : await producto.getAll();
-res.render('producto', {productos})
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-// router.get("/productos/:id", (req, res) => {
-//   let num = parseInt(req.params.id);
-//   if (isNaN(num) || num < 0) return res.status(400).send({ error: "ID INVALIDO" });
-//   producto.getById(num).then((resp) =>{
-//   if (resp === undefined ) return res.send({ error : 'producto no encontrado' });
-//   else res.send(`${JSON.stringify(resp)}`)
-// });
-// });
+router.get("/productos/:id", (req, res) => {
+  let num = parseInt(req.params.id);
+  if (isNaN(num) || num < 0)
+    return res.status(400).send({ error: "ID INVALIDO" });
+  producto.getById(num).then((resp) => {
+    if (resp === undefined)
+      return res.send({ error: "producto no encontrado" });
+    else res.send(`${JSON.stringify(resp)}`);
+  });
+});
 
 router.post("/productos", (req, res) => {
-producto.save(req.body).then(() => producto.getAll().then(()=>res.redirect("/api/")));
-console.log("PRODUCTO AGREGADO DE MANERA EXITOSA");
+  producto.save(req.body).then(() =>
+    producto.getAll().then(() => {
+      res.send(`PRODUCTO AGREGADO DE MANERA EXITOSA
+   <a href="http://localhost:8080/api/">VOLVER AL FORMULARIO DE CARGA
+    `);
+    })
+  );
 });
 
+router.put("/productos/:id", (req, res) => {
+  let num = parseInt(req.params.id);
+  if (isNaN(num) || num < 0)
+    return res.status(400).send({ error: "ID INVALIDO" });
+  let elementoEncontrado= arrayProd.find((elem) => elem.id===num
+  );
+  arrayProd.map(() => {
+    if (!elementoEncontrado) {
+      return res.status(400).send({ error: "ID INVALIDO" });
+    } else {
+      elementoEncontrado.title = req.body.title;
+      elementoEncontrado.price = req.body.price;
+      elementoEncontrado.thumbnail = req.body.thumbnail;
+    }
+  });
+  res.send(arrayProd);
+  fs.writeFileSync("productos.txt", JSON.stringify(arrayProd), "utf-8");
+});
 
-// router.put("/productos/:id", (req, res) => {
-//   let num = parseInt(req.params.id);
-//   if (isNaN(num) || num < 0)
-//     return res.status(400).send({ error: "ID INVALIDO" });
-//   let coincidencia = arrayProd.find((elem) => {
-//     if (elem.id !== num) return false;
-//     if (elem.id === num) return true;
-//   });
-//   arrayProd.map((prod) => {
-//     if (!coincidencia) {
-//       return res.status(400).send({ error: "ID INVALIDO" });
-//     } else {
-//       prod.title = req.body.title;
-//       prod.price = req.body.price;
-//       prod.thumbnail = req.body.thumbnail;
-//       return prod;
-//     }
-//   });
-//   res.send(arrayProd);
-//   fs.writeFileSync("productos.txt", JSON.stringify(arrayProd), "utf-8");
-// });
-
-// router.delete("/productos/:id", (req, res) => {
-//     let num = parseInt(req.params.id);
-//     if (isNaN(num) || num < 0) return res.status(400).send({ error: "ID INVALIDO" });
-//     producto.getById(num).then((resp) =>{
-//     if (resp === undefined ) return res.send({ error : 'producto no encontrado' });
-//     else{
-//     producto.deleteById(num)
-//     }})
-    //LE PUSE UN SET TIME OUT, PORQUE SINO ME DEVOLVIA UN JSON SIN ACTUALIZAR, 
-    //PERO ME BORRABA EL PRODUCTO DEL TXT
-//     setTimeout(() => {
-//         return producto.getAll()
-//         .then((resp) =>
-//         res.send(`
-//         ${JSON.stringify(resp)}
-//         `));
-//     }, 500);
-// });
-
+router.delete("/productos/:id", (req, res) => {
+  let num = parseInt(req.params.id);
+  if (isNaN(num) || num < 0)
+    return res.status(400).send({ error: "ID INVALIDO" });
+  producto.getById(num).then((resp) => {
+    if (resp === undefined)
+      return res.send({ error: "producto no encontrado" });
+    else {
+      producto.deleteById(num);
+    }
+  });
+  //LE PUSE UN SET TIME OUT, PORQUE SINO ME DEVOLVIA UN JSON SIN ACTUALIZAR,
+  //PERO ME BORRABA EL PRODUCTO DEL TXT
+  setTimeout(() => {
+    return producto.getAll().then((resp) =>
+      res.send(`
+        ${JSON.stringify(resp)}
+        `)
+    );
+  }, 500);
+});
 
 module.exports = router;
--- servidor.js
+++ servidor.js
@@ -1,72 +1,16 @@
 const express = require("express");
-const Contenedor = require("./index");
-const producto = new Contenedor("productos.txt");
-const fs = require("fs");
 const router = require("./router");
-const { engine } = require("express-handlebars");
-const { Server: HttpServer} = require("http");
-const { Server : SocketServer} = require("socket.io")
-const port = 8080;
 const app = express();
-const httpServer = new HttpServer(app)
-const socketServer = new SocketServer(httpServer) 
+const port = 8080;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 
-app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-    defaultLayout: "index.hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", "./public/hbs_views");
-
 app.use("/api", router);
 
-const saveMessages = (message)=>{
-const contenido = fs.readFileSync("messages.txt", "utf-8")
-if (contenido === "") {
-  fs.writeFileSync("messages.txt", JSON.stringify([message]))
-} else {
-  const messages = JSON.parse(contenido);
-  const newMessage = JSON.stringify([...messages,message])
-  fs.writeFileSync("messages.txt", newMessage)
-}
-}
+app.listen(port, ()=>{
+  console.log(`ESTOY ESCUCHANDO EN EL PUERTO ${port}`)
+})
 
-const readMessages = () => {
-  const contenido = fs.readFileSync('messages.txt','utf-8');
-  if (contenido === '') {
-      return 'MENSAJE VACIO';
-  } else {
-      return JSON.parse(contenido);
-  }
-}
-// Websockets 
-
-
-socketServer.on("connection", async socket =>{
-  console.log("NUEVO USUARIO CONECTADO")
-
-  socket.emit("productos", await producto.getAll())
-
-  socket.emit("messages", await readMessages());
-
-  socket.on("new_message", async (mensaje)=>{
-    saveMessages(mensaje);
-    socketServer.sockets.emit("messages", await readMessages());
-  })
-  socket.on("new_product", async (producto)=>{
-    let productos = await producto.getAll() === '' ? '' : await producto.getAll();
-    socketServer.sockets.emit("productos", productos)
-  })
-});
-
-httpServer.listen(port, () => {
-  console.log(`LEVANTANDO SERVIDOR en el puerto ${port}`);
-});
